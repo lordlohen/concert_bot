b7e775c13ad8863738e7764366f58ab10eb1f7d9
Ivan Prokhorov <lordlohen@gmail.com>
2022-02-19 11:03:53 +0300

    Fix date convert bug.

--- src/bot.py
+++ src/bot.py
@@ -11,6 +11,7 @@ import time
 
 locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 red = redis.Redis(host='localhost', port=6379, db=1, password=None, socket_timeout=None)
+redis_users = redis.Redis(host='localhost', port=6379, db=2, password=None, socket_timeout=None)
 
 token = '5055960243:AAE0ZNGoZnO0BeqEMxGnLrSf9jEyiCTlGR0'
 bot = telebot.TeleBot(token, threaded=True)
@@ -18,6 +19,7 @@ bot = telebot.TeleBot(token, threaded=True)
 greeting = "Приветствую, тебя путник. Я помогу тебе выбрать концерт в Питере.\n" \
            "Для поиска используй команду /search"
 
+choice_city = "Выберите город"
 choice_genre = "Выберите жанр мероприятия:"
 choice_date = "Введите диапозон дат (Пример: 01.01-31.12):"
 
@@ -29,18 +31,63 @@ genres = ['Фольклорные мероприятия', 'Танец', 'Юмо
           'Джаз', 'Народная/Фолк', 'Авторская/Шансон/Романсы', 'Хип-хоп/Рэп', 'Электронная музыка',
           'Творческий вечер', 'Другое', 'Клубы', 'Национальные мероприятия']
 
+cities = ['Москва', 'Санкт-Петербург', 'Абакан', 'Анапа', 'Архангельск', 'Астрахань', 'Барнаул', 'Белгород',
+          'Благовещенск', 'Брянск', 'Вел. Новгород', 'Владивосток', 'Владимир', 'Волгоград', 'Вологда', 'Воронеж',
+          'Геленджик', 'Екатеринбург', 'Иваново', 'Ижевск', 'Иркутск', 'Йошкар-Ола', 'Казань', 'Калининград', 'Калуга',
+          'Кемерово', 'Киров', 'Краснодар', 'Красноярск', 'Крым', 'Курск', 'Лазаревское', 'Липецк', 'Магнитогорск',
+          'Мурманск', 'Набережные Челны', 'Ниж. Новгород', 'Новокузнецк', 'Новороссийск', 'Новосибирск', 'Омск',
+          'Оренбург', 'Орёл', 'Орск', 'Пенза', 'Пермь', 'Петрозаводск', 'Псков', 'Ростов-на-Дону', 'Рязань', 'Самара',
+          'Саранск', 'Саратов', 'Смоленск', 'Сочи', 'Ставрополь', 'Старый оскол', 'Сургут', 'Тамбов', 'Тверь',
+          'Тольятти', 'Томск', 'Тула', 'Тюмень', 'Улан-Удэ', 'Ульяновск', 'Уфа', 'Хабаровск', 'Чайковский', 'Чебоксары',
+          'Челябинск', 'Череповец', 'Чита', 'Южно-Сахалинск', 'Ярославль']
+
+cities_code = ['msk', 'spb', 'aba', 'anapa', 'arh', 'astr', 'brn', 'belgorod', 'blag', 'bryansk', 'nov', 'vl', 'vlm',
+               'vlg', 'vologda', 'vrn', 'gel', 'ekb', 'ivanovo', 'izhevsk', 'irk', 'yola', 'kzn', 'kgd', 'klg',
+               'kemerovo', 'kirov', 'krd', 'krs', 'krym', 'kursk', 'lzr', 'lipetsk', 'mgn', 'murm', 'nabchelny', 'nn',
+               'novokuznetsk', 'nvrsk', 'nsk', 'omsk', 'orenburg', 'orel', 'orsk', 'pnz', 'perm', 'ptz', 'pskov', 'rnd',
+               'rzn', 'smr', 'saransk', 'saratov', 'smolensk', 'sochi', 'sk', 'oskol', 'sur', 'tambov', 'tver', 'tlt',
+               'tomsk', 'tula', 'tmn', 'ulan', 'ulyanovsk', 'ufa', 'hbr', 'chaik', 'cheboksary', 'chel', 'cher',
+               'chita', 'sakh', 'yar']
+
 
 class States:
     genre = 1
     date = 2
+    city_changed = 3
+    search = 4
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    if message.from_user.id not in redis_users.scan_iter(f'user:{message.from_user.id}'):
+        redis_users.set(name=f'user:{message.from_user.id}', value=message.from_user.id)
+    with bot.retrieve_data(message.from_user.id) as data:
+        data['city'] = ''
     bot.send_message(message.from_user.id, greeting)
 
 
-@bot.message_handler(commands="search")
+@bot.message_handler(commands="city")
+def city(message):
+    city_markup = types.ReplyKeyboardMarkup()
+    cities_btn = []
+    for c in range(len(cities)):
+        cities_btn.append(types.KeyboardButton(cities[c]))
+        city_markup.add(cities_btn[c])
+    bot.set_state(message.from_user.id, States.city_changed)
+    bot.send_message(message.from_user.id, choice_city, reply_markup=city_markup)
+
+
+@bot.message_handler(state=States.city_changed)
+def city_changed(message):
+    with bot.retrieve_data(message.from_user.id) as data:
+        data['city'] = cities_code[cities.index(message.text)]
+        print(data['city'])
+    markup = types.ReplyKeyboardRemove()
+    bot.set_state(message.from_user.id, States.search)
+    bot.send_message(message.from_user.id, "Город выбран!\n Введите /search что бы начать поиск!", reply_markup=markup)
+
+
+@bot.message_handler(commands="search", state=States.search)
 def search(message):
     genre_markup = types.ReplyKeyboardMarkup()
     genre_1 = types.KeyboardButton('Фольклорные мероприятия')
@@ -77,15 +124,18 @@ def get_genre(message):
     bot.set_state(message.from_user.id, States.date)
     with bot.retrieve_data(message.from_user.id) as data:
         data['genre'] = genres_code[genres.index(message.text)]
+        print(data['genre'])
 
 
 @bot.message_handler(state=States.date)
 def get_date(message):
     with bot.retrieve_data(message.from_user.id) as data:
         bot.send_message(message.from_user.id, f"Ищем билеты {data['genre']}...")
-        data['date'] = message.text
+        if message.text != '/search':
+            data['date'] = message.text
+        print(data['date'])
     # bot.register_next_step_handler(message, scrap)
-        scrap(message)
+    scrap(message)
 
 
 def scrap(message):
@@ -95,29 +145,38 @@ def scrap(message):
 
 
 def show_concerts(message):
-    bot.send_message(message.from_user.id, 'Got it')
     with bot.retrieve_data(message.from_user.id) as data:
+        print(data)
         d = []
-        datelist = []
+        dateList = []
 
-        bot.send_message(message.from_user.id, data['date'])
+        m_f = re.search(r'(3[01]|[12][0-9]|0?[1-9])\.(1[0-2]|0?[1-9])-', data['date']).group()[
+              :-1] + f'.{datetime.now().year}'
+        m_t = re.search(r'-(3[01]|[12][0-9]|0?[1-9])\.(1[0-2]|0?[1-9])', data['date']).group()[
+              1:] + f'.{datetime.now().year}'
 
-        m_f = re.search(r'(?:0?[1-9]|[12][0-9]|3[01]).(?:0?[1-9]|1[0-2])-', data['date']).group()[:-1] + '.22'
-        m_t = re.search(r'-(?:0?[1-9]|[12][0-9]|3[01]).(?:0?[1-9]|1[0-2])', data['date']).group()[1:] + '.22'
-        d_f = datetime.strptime(m_f, '%d.%m.%y').date()
-        d_t = datetime.strptime(m_t, '%d.%m.%y').date()
+        d_f = datetime.strptime(m_f, '%d.%m.%Y')  # .date()
+        d_t = datetime.strptime(m_t, '%d.%m.%Y')  # .date()
+
+        if d_t < d_f:
+            d_t = d_t.replace(year=d_t.year + 1)
+
+        d_f = d_f.date()
+        d_t = d_t.date()
 
         for day in pd.date_range(min(d_f, d_t), max(d_f, d_t)):
             day, month = day.strftime('%d %b').split(' ')
-            datelist.append(f'{day} {month.capitalize()}')
+            dateList.append(f'{day} {month.capitalize()}')
 
-        for date in datelist:
-            for key in red.scan_iter(f"{data['genre']}:{date}*:*"):
+        for date in dateList:
+
+            for key in red.scan_iter(f"{data['city']}*:{data['genre']}:{date}*:*"):
                 for i in red.hscan(key):
                     if i != 0:
                         d.append(i)
 
         for t in range(len(d)):
+            print('-', end='')
             img = open('out.jpg', 'wb')
             img.write(urllib.request.urlopen(d[t].get(b'img').decode('utf-8')).read())
             img.close()
@@ -140,9 +199,7 @@ def show_concerts(message):
 
 
 if __name__ == '__main__':
-
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.enable_saving_states()
 
     bot.polling(none_stop=True, interval=0)
-
